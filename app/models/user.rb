19b77f52d8ed59954acacbd30a3334e070d5d17e
kobayashi64 <youkoba64@gmail.com>
2022-03-03 11:57:43 +0900

    コンフリクトの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-<<<<<<< Updated upstream
-=======
 
          validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
 
          extend ActiveHash::Associations::ActiveRecordExtensions
          belongs_to :prefecture
          belongs_to :group
->>>>>>> Stashed changes
 end
