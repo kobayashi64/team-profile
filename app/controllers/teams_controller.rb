798891e425459a0496abcbc7df683e6cba9c4804
kobayashi64 <youkoba64@gmail.com>
2022-03-04 08:31:55 +0900

    メンバ一覧表示の設定

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,4 +1,5 @@
 class TeamsController < ApplicationController
   def index
+    @user = User.all
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_one_attached :image
+  has_one_attached :image
 
-         validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 
-         extend ActiveHash::Associations::ActiveRecordExtensions
-         belongs_to :prefecture
-         belongs_to :group
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+  belongs_to :group
 end
