a457b610ac9483dbbd654f576f472560aa97886e
kobayashi64 <youkoba64@gmail.com>
2022-03-05 10:44:35 +0900

    Viewファイルの修正

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,7 +1,7 @@
 class Group < ActiveHash::Base
   self.data = [
-    { id: 1, name: '---' }, { id: 2, name: 'A社' }, { id: 3, name: 'B社' },
-    { id: 4, name: 'C社' }, { id: 5, name: 'D社' }, { id: 6, name: 'E社' }
+    { id: 1, name: '---' }, { id: 2, name: 'Aグループ' }, { id: 3, name: 'Bグループ' },
+    { id: 4, name: 'Cグループ' }, { id: 5, name: 'Dグループ' }, { id: 6, name: 'Eグループ' }
   ]
 
   include ActiveHash::Associations
