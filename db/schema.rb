eb6443d49ecaff48d5e010222e56e0d2ee4a01b2
kobayashi64 <youkoba64@gmail.com>
2022-03-05 17:53:06 +0900

    AWS s3 画像保存の設定

--- db/schema.rb
+++ db/schema.rb
@@ -51,7 +51,5 @@ ActiveRecord::Schema.define(version: 2022_03_03_030954) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-
 end
