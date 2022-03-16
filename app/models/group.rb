f8ca4e85b941612d26ffe393ae23d1974f83b392
kobayashi64 <youkoba64@gmail.com>
2022-03-16 18:58:07 +0900

    commit

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,6 @@
 class Group < ActiveHash::Base
   self.data = [
-    { id: 1, name: '未定' }, { id: 2, name: 'Aグループ' }, { id: 3, name: 'Bグループ' },
+    { id: 1, name: '---' }, { id: 2, name: 'Aグループ' }, { id: 3, name: 'Bグループ' },
     { id: 4, name: 'Cグループ' }, { id: 5, name: 'Dグループ' }
   ]
 
