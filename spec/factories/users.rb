13d03b80a114c553032e91c1dad5c9d851ff1ca4
kobayashi64 <youkoba64@gmail.com>
2022-03-23 16:39:54 +0900

    空白削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,8 +2,15 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.last_name }
     email { Faker::Internet.free_email }
-    password = Faker::Internet.password(min_length: 6)
-    password { password }
+    introduction { Faker::Lorem.sentence }
+    password { Faker::Internet.password(min_length: 6)}
     password_confirmation { password }
+    group_id	{ Faker::Number.between(from: 1, to: 5) }
+    prefecture_id{ Faker::Number.between(from: 1, to: 48) }
+    birthday { '1980-01-01' }
+    assignment_date { '2015-04-01' }
+
+    after(:build) do |user|
+      user.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
   end
 end
