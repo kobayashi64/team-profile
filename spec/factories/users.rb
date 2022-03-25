011c41fac43bcbe13c5ee47eceaf368cabea2072
kobayashi64 <youkoba64@gmail.com>
2022-03-25 11:10:15 +0900

    viewファイル修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,15 +3,15 @@ FactoryBot.define do
     name { Faker::Name.last_name }
     email { Faker::Internet.free_email }
     introduction { Faker::Lorem.sentence }
-    password { Faker::Internet.password(min_length: 6)}
+    password { Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     group_id	{ Faker::Number.between(from: 1, to: 5) }
-    prefecture_id{ Faker::Number.between(from: 1, to: 48) }
+    prefecture_id { Faker::Number.between(from: 1, to: 48) }
     birthday { '1980-01-01' }
     assignment_date { '2015-04-01' }
 
     after(:build) do |user|
       user.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
-end
 end
